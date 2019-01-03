15a16e09326c94de996bb601f8dde2c66e8c9244
Andrew <andretis990@gmail.com>
2019-01-03 15:22:14 +0300

    Initial Commit

---
+++ TestIosFirst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestIosFirst
+//
+//  Created by Андрей Тищенко on 03.01.2019.
+//  Copyright © 2019 Me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
